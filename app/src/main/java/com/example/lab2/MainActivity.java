de7ec8c4a5249f820b07e04c31b5b5b549631a6a
Polina <pelepolya@yandex.ru>
2023-02-24 20:27:38 +0300

    feat: added ViewBinding

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -9,18 +9,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.lab2.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView title = findViewById(R.id.title);
-        title.setText(R.string.auth);
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.startframe_people_blueprint);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //TextView title = findViewById(R.id.title);
+        binding.title.setText(R.string.auth);
+        //ImageView imageView = findViewById(R.id.imageView);
+        binding.imageView.setImageResource(R.drawable.startframe_people_blueprint);
+        //Button button = findViewById(R.id.button);
+        binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("my logs", "button clicked");
