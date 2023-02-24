9c0677f2c980d99024e10fe5715e35d7ed40ac7c
Polina <pelepolya@yandex.ru>
2023-02-24 21:42:49 +0300

    feat: added invalid phone input check

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.lab2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +18,19 @@ import com.example.lab2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result != null && result.getResultCode() == RESULT_OK) {
+                        if (result.getData() != null && result.getData().getStringExtra(MenuActivity.NUMBER) != null) {
+                            binding.enterPhone.setText(result.getData().getStringExtra(MenuActivity.NUMBER).substring(2));
+                            binding.enterPasswd.setText("");
+                        }
+                    }
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d("my logs", "button clicked");
+                startForResult.launch(MenuActivity.newIntentMenuActivity(MainActivity.this,
+                        binding.enterPhone.getText().toString()));
             }
         });
     }
