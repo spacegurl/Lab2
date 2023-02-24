1e1ce00d4f997506566a035c35e7915a55c9e7fd
Polina <pelepolya@yandex.ru>
2023-02-24 20:12:58 +0300

    feat: added 2 click button variations

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
         title.setText(R.string.auth);
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.startframe_people_blueprint);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("my logs", "button clicked");
+            }
+        });
+    }
+
+    public void SingInClick(View view) {
+        Log.d("my logs", "button clicked");
     }
 }
