8417fc29bc09905a62a89076edb9c0880fb58296
Polina <pelepolya@yandex.ru>
2023-02-24 19:56:46 +0300

    feat: added initialization to main activity

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView title = findViewById(R.id.title);
+        title.setText(R.string.auth);
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.startframe_people_blueprint);
     }
 }
