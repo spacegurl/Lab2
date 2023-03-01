94dca1db7f89448aaddf7951227b917b20065106
Polina <pelepolya@yandex.ru>
2023-03-01 22:30:21 +0300

    feat: added SignUpFragment layout

---
+++ app/src/main/java/com/example/lab2/SignUpFragment.java
@@ -0,0 +1,28 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.lab2.databinding.FragmentSignUpBinding;
+
+public class SignUpFragment extends Fragment {
+    private FragmentSignUpBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentSignUpBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
