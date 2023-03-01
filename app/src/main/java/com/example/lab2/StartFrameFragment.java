853987cfc58bb8341ac289be586bb53ade3b3c18
Polina <pelepolya@yandex.ru>
2023-03-01 21:33:12 +0300

    feat: added StartFrameFragment layout

---
+++ app/src/main/java/com/example/lab2/StartFrameFragment.java
@@ -0,0 +1,29 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.lab2.databinding.FragmentStartFrameBinding;
+
+public class StartFrameFragment extends Fragment {
+    private FragmentStartFrameBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentStartFrameBinding.inflate(getLayoutInflater());
+        return binding.getRoot(); //какую разметку используем
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
