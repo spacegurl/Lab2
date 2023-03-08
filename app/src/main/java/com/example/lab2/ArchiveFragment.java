e2cf1589fffdac73cc3105c7429c5dce63c33520
Polina <pelepolya@yandex.ru>
2023-03-08 23:47:20 +0300

    feat: added Projects and Archive fragments (layouts and classes)

---
+++ app/src/main/java/com/example/lab2/ArchiveFragment.java
@@ -0,0 +1,30 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.lab2.databinding.FragmentArchiveBinding;
+
+public class ArchiveFragment extends Fragment {
+
+    private FragmentArchiveBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentArchiveBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
