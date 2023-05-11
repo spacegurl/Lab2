18202a56c88e6ca128526f2ebbefce44a90a520b
Polina <pelepolya@yandex.ru>
2023-05-12 00:26:48 +0300

    feat: added circle animation

--- app/src/main/java/com/example/lab2/ui/fragments/MenuFragment.java
+++ app/src/main/java/com/example/lab2/ui/fragments/MenuFragment.java
@@ -1,6 +1,8 @@
 package com.example.lab2.ui.fragments;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +32,15 @@ public class MenuFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) binding.circles.getDrawable();
+                drawable.start();
+            }
+        }, 5000);
+
         getArgs();
         binding.PROJECTSBUTTON.setOnClickListener(new View.OnClickListener() {
             @Override
